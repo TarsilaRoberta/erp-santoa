1e871c5f649430c4a76d25bb71d841a1d1645711
Gabriel Carvalho <maracaipe@gmail.com>
2016-04-02 15:15:49 -0300

    Alterando menu e estrutura do projeto

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pr-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Santoa</title>
+	<link rel="stylesheet" href="css/menu-adm-santoa.css">
+</head>
+<body>
+	<div class="logo">
+		<li><img src="img-santoa/logomenu.png" alt="Logo Santoa"/></li>
+	</div>
+	<div class="menu">
+		<nav class="menu-nav">
+			<ul class="list-menu">
+				<li class="icon"><a href="pedido-compra.html">Pedidos</a></li>
+				<li class="icon"><a href="cadastro-cliente.html">Clientes</a></li>
+				<li class="icon"><a href="cadastro-produto.html">Produtos</a></li>
+				<li class="icon"><a href="despesas.html">Despesas</a></li>
+				<li class="icon"><a href="estoque.html">Estoque</a></li>									
+			</ul>
+		</nav>
+	</div>
